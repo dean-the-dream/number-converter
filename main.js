b034932340cb446da0ad8468d050d636f52bf155
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-24 00:31:04 -0400

    update: add to javascript

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-let avengers = [1, 2, 3]
-console.log(avengers.splice(1))
+const modeButtonGroup = document.querySelector(".mode-switch-container");
+const modeButtons = document.querySelectorAll(".mode-switch");
+const inputField = document.querySelector(".number-submit-label input");
 
-for(keys of Object.keys(fullStack)){
-    console.log(keys)
-
+for(i in modeButtons){
+   modeButtons[i].addEventListener("click",chooseMode); 
 }
 
-\
-const myCar = (x) {
-    return 
+function chooseMode (e){
+    console.log(e)
+    modeButtons.forEach(element => {
+       element.style = null; 
+    });
+    if(e.target.className == "mode-switch"){
+        e.target.style = `background-color: #4E148C;
+        height:175px;
+        color: white;`
+        let mode = e.target.firstElementChild.nextElementSibling.innerText
+        inputField.value = `Enter a ${mode} to convert.`
+    }
+    
+
 }
