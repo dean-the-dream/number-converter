1d4b203025e438159efef96df719cb54878245a8
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-25 12:45:37 -0400

    update: start js to convert integer to numeral

---
+++ integer-to-numeral.js
@@ -0,0 +1,19 @@
+const input = 3;
+let remaining = input;
+const response = []
+const conversionChart = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+}
+
+// while(remaining > 0) {
+    for({x, y} in conversionChart){
+        console.log(x)
+    }
+// }
+
--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const modeButtonGroup = document.querySelector(".mode-switch-container");
 const modeButtons = document.querySelectorAll(".mode-switch");
 const inputField = document.querySelector(".number-submit-label input");
-
-for(i in modeButtons){
-   modeButtons[i].addEventListener("click",chooseMode); 
-}
+const form = document.querySelector("form");
 
 function chooseMode (e){
     console.log(e)
@@ -16,8 +13,30 @@ function chooseMode (e){
         height:175px;
         color: white;`
         let mode = e.target.firstElementChild.nextElementSibling.innerText
-        inputField.value = `Enter a ${mode} to convert.`
+        inputField.placeholder = `Enter a ${mode} to convert.`
+    } 
+    if (e.target.parentElement.className == "mode-switch"){
+        e.target.parentElement.style = `background-color: #4E148C;
+        height:175px;
+        color: white;`
+        let mode = e.target.parentElement.firstElementChild.nextElementSibling.innerText
+        inputField.placeholder = `Enter a ${mode} to convert.`
+    }
+}
+
+
+function pressConvert (e) {
+    console.log(e)
+    if(e.target.className == "submit"){
+        console.log(e)
+        e.preventDefault();
     }
     
-
 }
+
+
+for(i in modeButtons){
+   modeButtons[i].addEventListener("click",chooseMode); 
+}
+
+form.addEventListener("click",pressConvert);
